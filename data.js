c9ef0a3b5d7833560ba44ca67e2eb8585c69cfa2
Hamna Ahmad <104259677+Hamna-Ahmad@users.noreply.github.com>
2023-03-11 19:16:29 +1100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,32 @@
+const characterData = {
+    hero: {
+        name: "Jin Kazama",
+        avatar: "images/JinKazama.png",
+        health: 60,
+        diceCount: 3,
+        currentDiceScore: []
+    },
+    orc: {
+        name: "Feng Wei",
+        avatar: "images/FengWei.jpg",
+        health: 16,
+        diceCount: 1,
+        currentDiceScore: []
+    },
+    demon: {
+        name: "Paul Phoneix",
+        avatar: "images/Paul.png",
+        health: 22,
+        diceCount: 2,
+        currentDiceScore: []
+    },
+    goblin: {
+        name: "True Ogre",
+        avatar: "images/ogre.png",
+        health: 30,
+        diceCount: 3,
+        currentDiceScore: []
+    }
+}
+
+export default characterData
